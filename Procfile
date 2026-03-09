4369470efd0c454810c1fcb0fc5f7e01ebc761c4
aira <sairaikram1406@gmail.com>
2026-03-09 00:32:21 -0700

    clean railway deployment test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn security_dashboard.app:app --bind 0.0.0.0:$PORT
+web: gunicorn --bind 0.0.0.0:$PORT security_dashboard.app:app
--- app.py
+++ app.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask is running on Railway!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    return "Railway deployment works!"
