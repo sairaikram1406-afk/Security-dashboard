041d43a0ec7fc53a2b1e09a4147b09e524ea92ca
aira <sairaikram1406@gmail.com>
2026-03-08 23:15:53 -0700

    fix railway port binding

--- app.py
+++ app.py
@@ -47,5 +47,8 @@ def home():
     return render_template("index.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
