ab29fa56910f909fc6dad435bf9f27687f56e087
aira <sairaikram1406@gmail.com>
2026-03-11 10:37:31 -0700

    connect flask backend to dashboard

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from password_engine import analyze_password, PasswordGenerator
+from breach_checker import check_breach
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "HELLO FROM RAILWAY"
+generator = PasswordGenerator()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    generated_password = None
+    result = None
+    breach_count = None
+
+    if request.method == "POST":
+
+        generate_flag = request.form.get("generate")
+        password = request.form.get("password")
+        breach_check = request.form.get("breach_check")
+
+        # ---------- GENERATE PASSWORD ----------
+        if generate_flag == "yes":
+            generated = generator.generate_password(target_entropy_bits=100)
+            generated_password = generated["password"]
+
+            return render_template(
+                "index.html",
+                generated_password=generated_password
+            )
+
+        # ---------- ANALYZE PASSWORD ----------
+        if password:
+
+            result_data = analyze_password(password)
+
+            result = {
+                "score": result_data["score"],
+                "guesses": result_data["guesses"],
+                "crack_time": result_data["crack_time"],
+                "warning": result_data["feedback"]["warning"],
+                "suggestions": result_data["feedback"]["suggestions"]
+            }
+
+            # ---------- BREACH CHECK ----------
+            if breach_check == "yes":
+                breach_count = check_breach(password)
+
+            return render_template(
+                "result.html",
+                result=result,
+                breach_count=breach_count
+            )
+
+    return render_template(
+        "index.html",
+        generated_password=generated_password)
