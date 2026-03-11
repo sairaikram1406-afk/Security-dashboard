07ca408032048d9e380cd94eb82dc4c110ea8bec
aira <sairaikram1406@gmail.com>
2026-03-11 10:11:40 -0700

    diagnostic test for railway deployment

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Railway deployment works!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    return "HELLO FROM RAILWAY"
